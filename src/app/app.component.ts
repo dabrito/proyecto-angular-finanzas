fcf2667e14dda3824a13df4514f9145ed2865773
andreadelpinom <andreadelpino@uees.edu.ec>
2025-02-15 12:03:14 -0500

    Creacion componentes: dashboard, reportes, billeteras, conversion monedas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router'; 
+import { DashboardComponent } from './shared/dashboard/dashboard.component';
+import { ReportesComponent } from './shared/reportes/reportes.component';
+import { BilleterasComponent } from './shared/billeteras/billeteras.component'; 
+import { ConversionComponent } from './shared/conversion/conversion.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DashboardComponent, ReportesComponent,BilleterasComponent,ConversionComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] 
 })
 export class AppComponent {
   title = 'ProyectoDesarrolloWebP1';
 }
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+//import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './shared/dashboard/dashboard.component';
+import { ReportesComponent } from './shared/reportes/reportes.component';
+import { BilleterasComponent } from './shared/billeteras/billeteras.component';
+import { ConversionComponent } from './shared/conversion/conversion.component';
+
+export const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'reportes', component: ReportesComponent },
+  { path: 'billeteras', component: BilleterasComponent },
+  { path: 'conversion', component: ConversionComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' } // Redirección por defecto
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
