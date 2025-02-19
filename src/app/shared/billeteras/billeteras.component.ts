b0b8deff683b4d87446d1ccb561e14f5339d710e
dabrito <dylanbrito@uees.edu.ec>
2025-02-18 22:48:02 -0500

    Tabla de Billetera

--- src/app/shared/billeteras/billeteras.component.ts
+++ src/app/shared/billeteras/billeteras.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Transaccion } from '../../interfaz/transaccion';
 
 @Component({
   selector: 'app-billeteras',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './billeteras.component.html',
   styleUrl: './billeteras.component.css'
 })
-export class BilleterasComponent {
+export class BilleterasComponent implements OnInit {
 
+  transactions: Transaccion[] = [];
+
+  ngOnInit(): void {
+    this.loadTransactionsFromLocalStorage();
+  }
+
+  loadTransactionsFromLocalStorage(): void {
+    const data = localStorage.getItem('transactions');
+    if (data) {
+      this.transactions = JSON.parse(data);
+
+      this.transactions.forEach(t => {
+        t.date = new Date(t.date);
+      });
+    }
+  }
+
+  deleteTransaction(id: number): void {
+    this.transactions = this.transactions.filter(t => t.id !== id);
+    localStorage.setItem('transactions', JSON.stringify(this.transactions));
+  }
 }
